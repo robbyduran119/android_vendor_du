09f756fc4da706d71431d52c79896c041f67877d
storm119 <storm119@gmail.com>
2018-07-28 19:42:42 +0200

    add Whyred

--- caf-vendorsetup.sh
+++ caf-vendorsetup.sh
@@ -1,4 +1,4 @@
-for device in cheeseburger dumpling nash oneplus3 potter tenderloin; do
+for device in cheeseburger dumpling nash oneplus3 potter tenderloin whyred; do
     add_lunch_combo du_${device}-user
     add_lunch_combo du_${device}-userdebug
 done
