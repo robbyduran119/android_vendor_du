3aecf7f30be81124e68a71db6cf920bda8f794cb
storm119 <storm119@gmail.com>
2018-07-28 19:47:03 +0200

    May the GOD bless my ancestors...

--- config/branding.mk
+++ config/branding.mk
@@ -2,7 +2,7 @@
 DU_BASE_VERSION = v12.4
 
 ifndef DU_BUILD_TYPE
-    DU_BUILD_TYPE := UNOFFICIAL
+    DU_BUILD_TYPE := duRAN
 endif
 
 # Only include DU-Updater for official, weeklies, and rc builds
